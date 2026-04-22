b7fee8eb15d30c4d8f1232791ceb0a35fcd87da9
XenoxiousPoison <XenophageUser@gmail.com>
2026-04-22 16:40:46 -0400

    Menu button builds

--- Project_AXES/obj/Debug/net8.0/Project_AXES.AssemblyInfo.cs
+++ Project_AXES/obj/Debug/net8.0/Project_AXES.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_AXES")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ac8a5e8c5870f77117fc2a1984c703dd074dc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+668947bc45b38129b94138f700e8bab2534dfc01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_AXES")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_AXES")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
