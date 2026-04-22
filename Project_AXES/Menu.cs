668947bc45b38129b94138f700e8bab2534dfc01
XenoxiousPoison <XenophageUser@gmail.com>
2026-04-22 15:08:43 -0400

    Menu changes

--- Project_AXES/Menu.cs
+++ Project_AXES/Menu.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -10,13 +11,28 @@ namespace Project_AXES
 {
     internal class Menu
     {
+        // Drawing the menu to the screen
         private SpriteFont font;
+        private Texture2D title;
         private Texture2D button;
-        private List<Rectangle> buttonList;
+
+        // Utilized for button positioning
         private int buttonOffset;
-        public Menu(SpriteFont font)
+        private int windowWidth;
+        private int windowHeight;
+
+        //
+        private List<Rectangle> buttonList;
+        private MouseState mouse;
+        private MouseState previousMouse;
+        public Menu(SpriteFont font, Texture2D button, Texture2D title, int windowWidth, int windowHeight)
         {
-            
+            this.title = title;
+            this.button = button;
+            this.font = font;
+            this.windowWidth = windowWidth;
+            this.windowHeight = windowHeight;
+
         }
         public void Draw()
         {
