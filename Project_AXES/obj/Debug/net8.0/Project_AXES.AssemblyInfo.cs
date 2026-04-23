1b9b964ce686d0f090e0e3b77773f4d62b10f167
XenoxiousPoison <XenophageUser@gmail.com>
2026-04-22 23:42:04 -0400

    Fixed bug with obj folder preventing VS 2022 from running the project!

--- Project_AXES/obj/Debug/net8.0/Project_AXES.AssemblyInfo.cs
+++ Project_AXES/obj/Debug/net8.0/Project_AXES.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_AXES")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca629431fa90d6bc9a587af848da6253fa352e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f72cd1d516bca95ee81e06a3ee0fd1e4bd05354")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_AXES")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_AXES")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
